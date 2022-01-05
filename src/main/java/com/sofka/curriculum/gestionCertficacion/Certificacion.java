09f870145427429564a20ab4546a4e021a0de2a6
juanfvalencia <90592176+juanfvalencia@users.noreply.github.com>
2022-01-05 09:49:10 -0500

    Se configura la entidad Certificacion con sus respectivos comportamientos

--- src/main/java/com/sofka/curriculum/gestionCertficacion/Certificacion.java
+++ src/main/java/com/sofka/curriculum/gestionCertficacion/Certificacion.java
@@ -1,4 +1,48 @@
 package com.sofka.curriculum.gestionCertficacion;
 
-public class Certificacion {
+import co.com.sofka.domain.generic.Entity;
+import com.sofka.curriculum.generics.Institucion;
+import com.sofka.curriculum.generics.Periodo;
+import com.sofka.curriculum.gestionCertficacion.value.CertificacionId;
+import com.sofka.curriculum.gestionCertficacion.value.Nombre;
+
+import java.util.Objects;
+
+public class Certificacion extends Entity<CertificacionId> {
+
+    protected Nombre nombre;
+    protected Institucion institucion;
+    protected Periodo periodo;
+
+    public Certificacion(CertificacionId entityId, Nombre nombre, Institucion institucion, Periodo periodo) {
+        super(entityId);
+        this.nombre = nombre;
+        this.institucion = institucion;
+        this.periodo = periodo;
+    }
+
+    public void modificarNombre(Nombre nombre){
+        this.nombre = Objects.requireNonNull(nombre);
+    }
+
+    public void modificarInstitucion(Institucion institucion){
+        this.institucion = Objects.requireNonNull(institucion);
+    }
+
+    public void modificarPeriodo(Periodo periodo){
+        this.periodo = Objects.requireNonNull(periodo);
+    }
+
+    public Nombre nombre(){
+        return nombre;
+    }
+
+    public Institucion institucion(){
+        return institucion;
+    }
+
+    public Periodo periodo(){
+        return periodo;
+    }
 }
+
